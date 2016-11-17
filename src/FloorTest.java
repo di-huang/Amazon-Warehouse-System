acecd4d9fae9d9a13d6b7104bbf2a23a017f51ba
Joseph Leiferman <joseph.leiferman@gmail.com>
2016-11-17 15:05:46 -0600

    added Floor related files

---
+++ src/FloorTest.java
@@ -0,0 +1,62 @@
+
+package warehouse_system.floor;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.ArrayList;
+
+/**
+ * @author josephtleiferman
+ *
+ */
+public class FloorTest {
+	MockFloor f = new MockFloor();
+	Point p = new Point(2,5,"p");
+	
+	@Test
+	public void test() {
+		//testing Point class
+		int [] t1 = p.getPoint();
+		int[] tt1 = {2,5};
+		assertArrayEquals(tt1,t1);
+		p.setPoint(5,2);
+		int[] tt2 = {5,2};
+		assertArrayEquals(tt2,p.getPoint());
+		
+		// testing getLocation
+		String tester = "SHELVE_1";
+		Point expected = new Point(2,2,"expected");
+		Point got  = f.getLocation(tester);
+		assertArrayEquals(expected.getPoint(),got.getPoint());
+		//testing getLocation
+		Point expected1 = new Point(1,5,"expected1");
+		tester = "PICKER";
+		got = f.getLocation(tester);
+		assertArrayEquals(expected1.getPoint() ,got.getPoint());
+		// testing objectAt
+		boolean got1 = f.objectAt(expected);
+		assertEquals(true,got1);
+		//testing objectAt
+		got1 = f.objectAt(expected1);
+		//testing objectAt
+		assertEquals(true,got1);
+		//testing objectAt
+		Point notInFloor = new Point(0,0,"notInFloor");
+		assertEquals(false,f.objectAt(notInFloor));
+		//testing updateShelve
+		f.updateObjectLocation("SHELVE_1", notInFloor);
+		assertEquals(notInFloor,f.FLOOR_LOCATIONS.get("SHELVE_1"));
+		//testing get route
+		ArrayList<MockFloor.Directions> expectedRoute = new ArrayList<>();
+		Point shelve1 = new Point(2,2,"shelve_1");
+		Point charger2 = new Point(3,0,"charger2");
+		expectedRoute.add(MockFloor.Directions.RIGHT);
+		expectedRoute.add(MockFloor.Directions.UP);
+		expectedRoute.add(MockFloor.Directions.UP);
+		assertEquals(expectedRoute, Floor.getRoute(shelve1,charger2));
+		
+		
+	}
+	
+
+}
---
+++ src/Shelve.java
@@ -0,0 +1,50 @@
+package warehouse_system.floor;
+import java.util.HashMap;
+/**
+ * 
+ * @author josephtleiferman
+ *
+ */
+public class Shelve extends wareHouseLocation {
+	
+	private HashMap<String,Item> itemsOnShelves = new HashMap<>();
+	private Point location; 
+	public Shelve(Point p) {
+		super(p);
+		
+	}
+
+	public Point getLocation() {
+		return location;
+	}
+	public void setLocatoin(int x, int y) {
+		location.setPoint(x,y);
+	}
+	public void put(String n, Item i) {
+		itemsOnShelves.put(n, i);
+	}
+	public void remove(String n) {
+		itemsOnShelves.remove(n);
+	}
+}
+class Item {
+	private double weight;
+	private String name;
+	
+	public Item(double weight,String name) {
+		this.weight = weight;
+		this.name = name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getWeight() {
+		return weight;
+	}
+}
