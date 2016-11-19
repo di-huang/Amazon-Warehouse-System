cd90c0117650cc857d43b740c341197e7196bfe6
Wei <Wei@192.168.3.177>
2016-11-18 20:11:45 -0600

    new Production.java for 15Nov

---
+++ src/production/Production.java
@@ -0,0 +1,14 @@
+package production;
+/**
+ * 
+ * @author Wei Gui
+ * 
+ */
+public class Production {
+	// Simply run the TestMaster test
+	public static void main(String[] args) {
+		TestMaster test=new TestMaster();
+		test.test000();
+	}
+
+}
