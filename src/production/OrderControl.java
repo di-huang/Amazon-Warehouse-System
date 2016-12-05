577a9d1e97da50bbc58075d6b05beb0f3980c6bf
cmcarlsn <cmcarlsn@CS-S129.iowa.uiowa.edu>
2016-12-05 12:33:47 -0600

    Added mock orders

--- src/production/OrderControl.java
+++ src/production/OrderControl.java
@@ -1,26 +1,53 @@
 package production;
 
-import java.util.LinkedList;
 
-import production.Tickable;
-import production.ItemController;
-import production.RobotScheduler;
+import java.util.*;
 
-public class OrderControl implements Tickable, Orders, Picker {
+
+import production.*;
+
+
+public class OrderControl implements Tickable {
 	
 	/**
 	 * 
-	 * orders are held in a queue
+	 * Orders are held in a queue
+	 * mock Orders are kept in orderQueueTesting and are popped off and
+	 * pushed onto the orderQueue with each tick
 	 * 
 	 */
-	private LinkedList<Order> orderQueue = new LinkedList<>();
-	private ItemController itemController;
-	private RobotScheduler robotScheduler;
-	private Belt belt;
-	private Bin currentBin;
-	private Item neededItem;
-
-
+	ArrayList<Order> orderQueueTesting = new ArrayList<Order>();
+	private LinkedList<Order> orderQueue;
+	private ItemController I;
+	private RobotScheduler R;
+	private Order currentOrder;
+	
+	/**
+	 * 
+	 * Mock Orders
+	 * 
+	 */
+	Item RED_SHIRT = new Item("RED_SHIRT", 200, "1");
+	Item BLUE_SHIRT = new Item("BLUE_SHIRT", 300, "1");
+	Item BICYCLE = new Item("BICYCLE", 2, "2");
+	Item BASKETBALL = new Item("BASKETBALL", 50, "2");
+	Item SHAVING_CREAM= new Item("SHAVING_CREAM", 10, "3");
+	Item DUCT_TAPE = new Item("DUCT_TAPE", 1, "3");
+	Item BOOK = new Item("BOOK", 20, "4");
+	Item EGG_PLANT = new Item("EGG_PLANT", 2000, "5");
+	Item FLUTE = new Item("FLUTE", 5, "6");
+	
+	
+	Order order1 = new Order(RED_SHIRT, 2, "501 South Dodge Street");
+	Order order2 = new Order(BLUE_SHIRT, 5, "313 South Gilbert Street");
+	Order order3 = new Order(BICYCLE, 1, "613 East Court Street");
+	Order order4 = new Order(BASKETBALL, 2, "512 North Dodge Street");
+	Order order5 = new Order(SHAVING_CREAM, 4, "902 Davenport Street");
+	Order order6 = new Order(DUCT_TAPE, 1, "451 East Market Street");
+	Order order7 = new Order(BOOK, 1, "222 Governor Street");
+	Order order8 = new Order(EGG_PLANT, 1, "378 Lucas Street");
+	Order order9 = new Order(FLUTE, 1, "987 Clinton Ave");
+	
 	/**
 	 * 
 	 * @author Charles Carlson
@@ -30,56 +57,68 @@ public class OrderControl implements Tickable, Orders, Picker {
 	 * @param itemController
 	 * @param robotScheduler
 	 */
-	public OrderControl(ItemController itemController, RobotScheduler robotScheduler) {
-		this.itemController = itemController;
-		this.robotScheduler = robotScheduler;
+	public OrderControl(ItemController I, RobotScheduler R) {
+		this.I = I;
+		this.R = R;
 		
-		/**
-		 * @author Ted Herman
-		 * 
-		 */
-		for(int i=0;i<3;i++) {
-			orderQueue.addLast(getRandomOrder());
+		orderQueue = new LinkedList<>();
 		}
-	}
 	
 	/**
-	 * adds an order object to the queue of orders
-	 * 
 	 * @author Charles Carlson
-	 * @param orderToBeAdded
-	 */
-	public void addOrder(Order orderToBeAdded) {
-		orderQueue.add(orderToBeAdded);
-	}
-
-	/**
-	 * @author Charles Carlson
+	 * 1. 	Check if queue of orders is empty
+	 * 		if so:
+	 * 			pop an order from orderQueueTesting and add it to orderQueue
+	 * 2.	Check if the current order being processed is NULL
+	 * 		if so:
+	 * 			current order becomes first Order popped off of order queue
+	 * 			Print statement 
+	 * 3.	Check if robot is available:
+	 * 		if not:	
+	 * 			tick
+	 * 		if so:
+	 * 			give robot shelfID and request robot to fetch
+	 * 4.	tick
+	 * 			
 	 * 
-	 * as the master clock ticks orders are taken taken from the queue by the "picker"
-	 * 1.	Checks if the Item of specified quantity of the Order is available in the inventory
-	 * 		Retrieve the location (shelfID/itemShelf) to be given to the robot
-	 * 
-	 *  
 	 */
-	@Override
 	public void tick(int tick) {
-		if(orderQueue.isEmpty()) {
-			System.out.println("There are no orders");
-			return;
+		
+		orderQueueTesting.add(order1);
+		orderQueueTesting.add(order2);
+		orderQueueTesting.add(order3);
+		orderQueueTesting.add(order4);
+		orderQueueTesting.add(order5);
+		orderQueueTesting.add(order6);
+		orderQueueTesting.add(order7);
+		orderQueueTesting.add(order8);
+		orderQueueTesting.add(order9);
+
+
+		if (orderQueue.size() < 1) {
+			orderQueue.push(orderQueueTesting.get(0));
+			orderQueueTesting.remove(orderQueueTesting.get(0));
+			return; //if orderQueue empty, add from orderQueueTesting
 		}
 		
-		System.out.println("Order:" + orderQueue.peek().getItemBeingOrderedNameString());
-		if(itemController.itemAvailable(orderQueue.peek().getItemBeingOrdered())) {
-			
-			String itemShelf = orderQueue.peek().getItemBeingOrderedShelfID();
-			orderQueue.poll();
-			
-			
+		if(currentOrder == null) {
+			currentOrder = orderQueue.pop();
+			System.out.println("OrderControll starts a new Order");
+			//set currentOrder to first Order in OrderQueue
 		}
-		else {
-			System.out.println("There are no orders");
-			orderQueue.poll();
+		
+		if(R.getAvailableRobot() == null) {
+			return; //wait until later tick
 		}
+	
+		String currentOrderShelf = currentOrder.getItemBeingOrderedShelfID();
+		if(currentOrderShelf == null) { 
+			System.out.println("Item not in inventory");
+			return; //tick
+		}
+		
+		R.requestShelf(currentOrderShelf,(Picker)this);
+		return;//tick
+		
 	}
 }
