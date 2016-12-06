abb62fda2257a1c022a8d2202ad36f09ca8a8869
Bryant-Xu <wangyang-xu@uiowa.edu>
2016-12-06 10:32:01 -0600

    Add files via upload

--- src/production_unitTesting/Item.java
+++ src/production_unitTesting/Item.java
@@ -1,34 +1,71 @@
-package production_unitTesting;
-
+package production;
 
 /**
- * @author Wangyang Xu
- * establish an item database, including the basic imformation about the item.
+ * @author wangyang xu, dihuang
+ * member variables of Item: itemInfo, (shelf)holder
  */
- 
-
 public class Item {
-	
-	public final String name;
-	public int weight;
-	public int[] size;			// length, width, height
-	public int quantity;
-	public String shelfID;
-	public int row;
-	
-	public Item(String name, int quantity, String shelfID) {
-		this.name = name;
-		//this.weight = weight;
-		//this.size = size;
-		this.quantity = quantity;
-		this.shelfID = shelfID;
-		//this.row = row;
+	final ItemInfo itemInfo;
+	Shelf holder;
+	Item(ItemInfo info) {
+		itemInfo = info;
+		holder = null;
 	}
-	
+	int ID(){
+		return itemInfo.ID;
+	}
+	String description(){
+		return itemInfo.description;
+	}
+	Shelf getHolder() {
+		return holder;
+	}
+	void setHolder(Shelf holder) {
+		this.holder = holder;
+	}
+	boolean match(ItemInfo info) {
+		return itemInfo.equals(info);
+	}
+	boolean match(ItemSlot slot){
+		return itemInfo.equals(slot.itemInfo);
+	}
 	@Override
 	public String toString() {
-		return this.name;
-		
+		return itemInfo.toString();
 	}
+}
 
+/**
+ * @author wangyang xu, dihuang
+ * member variables of ItemSlot: itemInfo, item(being hold)
+ */
+class ItemSlot {
+	final ItemInfo itemInfo;
+	Item item;
+	ItemSlot(ItemInfo info) {
+		itemInfo = info;
+		item = null;
+	}
+	int ID(){
+		return itemInfo.ID;
+	}
+	String description(){
+		return itemInfo.description;
+	}
+	Item getItem() {
+		return item;
+	}
+	void setItem(Item i) {
+		item = i;
+	}
+	boolean match(ItemInfo info) {
+		return itemInfo.equals(info);
+	}
+	boolean match(Item item) {
+		return itemInfo.equals(item.itemInfo);
+	}
+	@Override
+	public String toString() {
+		return itemInfo.toString();
+	}
 }
