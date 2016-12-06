09c08c3cae24f2f191842238e2c8538ca328e535
Wei <Wei@192.168.3.177>
2016-12-06 12:13:31 -0600

    Main method in the production_unitTesting is being commented out.

--- src/production_unitTesting/Production.java
+++ src/production_unitTesting/Production.java
@@ -8,28 +8,28 @@ package production_unitTesting;
  */
 public class Production {
 
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
 		// Doing unit test
-		MockFloor F = new MockFloor();
-		MockBelt B = new MockBelt(F);
-		RobotScheduler R = new RobotScheduler(2);
-		Point robotP = new Point(0,3);
-		MockRobot r001= new MockRobot("001", robotP, R );
-		R.addRobot(r001); 
-		robotP.setPoint(3, 0);
-		MockRobot r002= new MockRobot("002", robotP, R );
-		R.addRobot(r002);
+		//MockFloor F = new MockFloor();
+		//MockBelt B = new MockBelt(F);
+		//RobotScheduler R = new RobotScheduler(2);
+		//Point robotP = new Point(0,3);
+		//MockRobot r001= new MockRobot("001", robotP, R );
+		//R.addRobot(r001); 
+		//robotP.setPoint(3, 0);
+		//MockRobot r002= new MockRobot("002", robotP, R );
+		//R.addRobot(r002);
 		
-		ItemController I = new ItemController();
-		I.addItem(new Item("apple", 5, "777"));
-		I.addItem(new Item("banana", 5, "778"));
+		//ItemController I = new ItemController();
+		//I.addItem(new Item("apple", 5, "777"));
+		//I.addItem(new Item("banana", 5, "778"));
 		
 		/**
 		 * adds 9 items to inventory
 		 * 
 		 */
 		
-		I.addItem(new Item("RED_SHIRT", 200, "1"));
+		/*I.addItem(new Item("RED_SHIRT", 200, "1"));
 		I.addItem(new Item("BLUE_SHIRT", 300, "1"));
 		I.addItem(new Item("BICYCLE", 2, "2"));
 		I.addItem(new Item("BASKETBALL", 50, "2"));
@@ -49,6 +49,6 @@ public class Production {
 		M.setUnitTime(0);
 		M.start();
 
-	}
+	}*/
 
 }
