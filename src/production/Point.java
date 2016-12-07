a8c621240b82b941515a76ef811b0731c8958205
dhuang12138 <di-huang@uiowa.edu>
2016-12-07 07:49:33 -0600

    Make Point compatible with Floor

--- src/production/Point.java
+++ src/production/Point.java
@@ -43,6 +43,50 @@ public class Point {
 	public boolean equals(Point other) {
 		return x == other.getX() && y == other.getY();
 	}
+	public Point nextpoint(Directions d){
+		switch(d){
+			case DOWN:
+				return new Point(x, y+1);
+			case UP:
+				return new Point(x, y-1);
+			case LEFT:
+				return new Point(x-1, y);
+			case RIGHT:
+				return new Point(x+1, y);
+			default:
+				return null;		// stay when d is null
+		}
+	}
+	public boolean out(){
+		return x < 0 || y < 0;
+	}
+	public boolean closeTo(Directions d, Point end){
+		Point next = this.nextpoint(d);
+		double nX = (next.getX()-end.getX()) * (next.getX()-end.getX());
+		double nY = (next.getY()-end.getY()) * (next.getY()-end.getY());
+		double nDis =  Math.sqrt(nX+nY);
+		double cX = (this.x-end.getX()) * (this.getX()-end.getX());
+		double cY = (this.y-end.getY()) * (this.y-end.getY());
+		double cDis =  Math.sqrt(cX+cY);
+		if(nDis<cDis){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public boolean closeTo(Point next, Point end){
+		double nX = (next.getX()-end.getX()) * (next.getX()-end.getX());
+		double nY = (next.getY()-end.getY()) * (next.getY()-end.getY());
+		double nDis =  Math.sqrt(nX+nY);
+		double cX = (this.x-end.getX()) * (this.getX()-end.getX());
+		double cY = (this.y-end.getY()) * (this.y-end.getY());
+		double cDis =  Math.sqrt(cX+cY);
+		if(nDis<cDis){
+			return true;
+		}else{
+			return false;
+		}
+	}
 	@Override
 	public String toString() {
 		return "(" + x + "," + y + ")";
